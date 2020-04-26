e1530bc86152e3b93ab99971e441c4003bc5efa7
yamming <minglok.yam@mail.utoronto.ca>
2020-04-26 01:30:34 -0400

    Updates

---
+++ precache-manifest.728c4861d133217a75cff1e7d661debd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43480b13a9ebb10bc8a4ee4aec417627",
+    "url": "/Practice/index.html"
+  },
+  {
+    "revision": "29913bbcd223f87e195b",
+    "url": "/Practice/static/css/main.16186a6e.chunk.css"
+  },
+  {
+    "revision": "9e9be3ce6dd30263ec4c",
+    "url": "/Practice/static/js/2.857faccc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Practice/static/js/2.857faccc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29913bbcd223f87e195b",
+    "url": "/Practice/static/js/main.77eb2e88.chunk.js"
+  },
+  {
+    "revision": "ff7a2d3f4dc35393b888",
+    "url": "/Practice/static/js/runtime-main.f2ac47a9.js"
+  }
+]);
